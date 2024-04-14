c7c0e9b87b275d8d7b971f18c43127cf6a0190ac
Juan José Muñoz Noda <61985033+JoseCoscu@users.noreply.github.com>
2024-04-13 22:44:04 -0400

    fixing lines

--- timer.py
+++ timer.py
@@ -19,9 +19,3 @@ class Time:
         self.second = (total_seconds % 3600) % 60
 
         print(self.hour, self.minute, self.second)
-arr=[1,2,3,4,5,6,7,8,9,10]
-
-if(12 in arr):
-    print("yes")
-else:
-    print("no")
