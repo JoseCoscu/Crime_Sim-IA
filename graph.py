51772653b51431053f94211a4bb9f8bce260bcfe
Juan José Muñoz Noda <61985033+JoseCoscu@users.noreply.github.com>
2024-04-22 17:51:17 -0400

    requirements.txt

--- graph.py
+++ graph.py
@@ -1,7 +1,5 @@
 import networkx as nx
-import matplotlib.pyplot as plt
-import networkx as nx
-import random
+
 import math
 import heapq
 
--- timer.py
+++ timer.py
@@ -36,24 +36,3 @@ class MiClase:
             time.sleep(i)
 
 
-# Creamos un objeto para el medidor de tiempo
-# time_meter = TimeMeter()
-#
-# # Creamos un hilo para actualizar el tiempo
-# time_updater_thread = threading.Thread(target=time_updater, args=(time_meter,))
-# time_updater_thread.daemon = True  # El hilo se detendrá cuando el programa principal termine
-# time_updater_thread.start()
-
-# # Creamos instancias de las clases y las ejecutamos en hilos diferentes
-# clase1 = MiClase("Clase1", time_meter)
-# clase2 = MiClase("Clase2", time_meter)
-#
-# thread1 = threading.Thread(target=clase1.do_something)
-# thread2 = threading.Thread(target=clase2.do_something)
-#
-# thread1.start()
-# thread2.start()
-#
-# # Esperamos a que los hilos terminen
-# thread1.join()
-# thread2.join()
